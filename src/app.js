dc4b8dd7291e7ef6a3af550df5f09933c5cfed3f
hfaujdar58 <hfaujdar58@gmail.com>
2024-08-20 03:44:21 +0000

    Added : Register controller and route

--- src/app.js
+++ src/app.js
@@ -10,11 +10,16 @@ app.use(cors({
 })); //use method is used for middlewares
 
 app.use(express.json({limit : "16kb"})); //if data comes from a form, in json form
-
 app.use(express.urlencoded()); // if data comes from url
-
 app.use(express.static("Public")); //configure express to handle static assets in Public folder
-
 app.use(cookieParser());
+
+//routes imports
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+//we will have to use as middlewares, this is the pattern
+app.use("/api/v1/users", userRouter);
+//http://localhost:4000/api/v1/users/register
 
 export default app;
